3185d97bfca3ca288cc9c198df278dd9399790b3
Suhodeeva-DV <81247221+Suhodeeva-DV@users.noreply.github.com>
2021-03-28 21:08:00 +0700

    Add files via upload

---
+++ Calculator/CalculatorModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public CalculatorModel()
+        {
+
+
+        }
+
+        public object Calculate(int operand1, int operand2, char operation)
+        {
+            double result = 0;
+            if (operation == '+')
+            {
+                result = operand1 + operand2;
+            }
+            else if (operation == '-')
+            {
+                result = operand1 - operand2;
+            }
+            else if (operation == '/')
+            {
+                result = operand1 / (double)operand2;
+            }
+            else
+            {
+                result = operand1 * operand2;
+            }
+
+            return result;
+        }
+
+
+    }
+}
